de296f4838e3f6aa49004075c580e7e8eb9364ab
FahadAminShovon <shovonfahad991@gmail.com>
2022-01-31 15:54:39 +0600

    :chore: rename word repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { Modal } from '../components/Modal/Modal';
-import { Block } from '../components/Word';
+import { Block } from '../components/Word1';
 import {
   CORRECT_WORD,
   NUMBER_OF_COLUMNS,
